ad4e82401a64102e001a730c306f8ef189bfaca8
Mandarina <idcareg.g@gmail.com>
2022-04-25 21:55:32 -0500

    Male and Female button 2

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -21,40 +21,12 @@ class InputPage extends StatelessWidget {
               children: [
                 Expanded(
                   child: ReusableCard(
-                    childCard: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        FaIcon(FontAwesomeIcons.mars, size: 60.0),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        Text(
-                          "MALE",
-                          style: TextStyle(
-                            fontSize: 22.0,
-                          ),
-                        ),
-                      ],
-                    ),
+                    childCard: IconContent(),
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
-                    childCard: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        FaIcon(FontAwesomeIcons.venus, size: 60.0),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        Text(
-                          "FEMALE",
-                          style: TextStyle(
-                            fontSize: 22.0,
-                          ),
-                        ),
-                      ],
-                    ),
+                    childCard: IconContent(),
                   ),
                 ),
               ],
@@ -106,6 +78,31 @@ class InputPage extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class IconContent extends StatelessWidget {
+  const IconContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        FaIcon(FontAwesomeIcons.mars, size: 60.0),
+        SizedBox(
+          height: 10.0,
+        ),
+        Text(
+          "MALE",
+          style: TextStyle(
+            fontSize: 22.0,
+          ),
+        ),
+      ],
     );
   }
 }
