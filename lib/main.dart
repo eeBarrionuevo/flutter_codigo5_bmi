752b278e23a2833f7c92df7a597abdc4a8f979f2
Mandarina <idcareg.g@gmail.com>
2022-04-25 19:45:22 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_codigo5_bmi/pages/input_page.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: InputPage(),
+    );
+  }
+}
+
+
+
+
+
---
+++ lib/pages/input_page.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+class InputPage extends StatelessWidget {
+  const InputPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Text("BMI Calculator"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
