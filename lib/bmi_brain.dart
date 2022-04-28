e23d526e40b920c6ffd5adad2becdd2a6d20240e
Mandarina <idcareg.g@gmail.com>
2022-04-27 19:29:12 -0500

    BMI Brain 1

---
+++ lib/bmi_brain.dart
@@ -0,0 +1,20 @@
+
+import 'dart:math';
+
+class BMIBrain {
+  int weight;
+  int height;
+
+  double bmi = 0;
+
+  BMIBrain({
+    required this.weight,
+    required this.height,
+  });
+
+  double calculateBMI() {
+    //bmi
+    bmi = weight / pow(height/100, 2);
+    return bmi;
+  }
+}
